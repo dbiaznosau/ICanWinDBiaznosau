74eb66b9281602f165cad4bcfce81cb4b346c9db
diaznosau <bezmusic@gmail.com>
2021-01-27 23:20:43 +0300

    Code cleanup Added Assertion

---
+++ src/test/java/page/ResultPage.java
@@ -0,0 +1,22 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ResultPage {
+    public WebDriver driver;
+    public ResultPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
+
+    }
+    @FindBy(xpath = "//*[@class='textarea']" )
+    WebElement pastedCodeField;
+
+    public String pastedCodeText() {
+        return pastedCodeField.getText();
+    }
+
+}
